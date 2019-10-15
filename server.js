35b19c4319dc5edd3b5558f766a22ab77ccad41e
jumodada <864353360@qq.com>
2019-10-15 22:27:17 +0800

    update

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
-const upload = multer({dest: 'docs/uploads'})
+const upload = multer({dest: 'uploads/'})
 const app = express()
 
-
-app.options('/upload',cors())
-app.get('/', (req, res, next) => {
-    res.send('Hello World')
-})
 
 app.get('/get', (req, res, next) => {
     res.send('get some message')
 })
+app.options('/upload',cors())
+
+
 app.post('/upload',cors(),upload.single('file'), (req, res, next) => {
-    res.json(req.file.filename)
+    res.send(req.file.filename)
 })
 
 app.get('/preview/:key',cors(), (req, res) => {
-    let str = `${req.params.key}`
-    str = str.slice(1,str.length-1)
-    res.sendFile(`uploads/${str}`,{
+    res.sendFile(`uploads/${req.params.key}`,{
         root:__dirname,
         headers:{
             'Content-Type':'image/jpeg'
@@ -29,4 +25,5 @@ app.get('/preview/:key',cors(), (req, res) => {
 })
 
 let port = process.env.PORT || 3000
+console.log(port)
 app.listen(port)
